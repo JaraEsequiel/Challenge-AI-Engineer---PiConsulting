49d47890c3dfca25be2dbae7986b6c846453ae64
EsequielJara <esequieljara2002@gmail.com>
2024-11-19 22:30:54 -0300

    llm routes and service

--- backend/app/api/services/llm_service.py
+++ backend/app/api/services/llm_service.py
@@ -1,4 +1,19 @@
+from langchain_openai import ChatOpenAI
+
 class LLMService:
+    _llm: ChatOpenAI
+
+    async def init_llm(self, provider: str, model: str):
+        if provider.lower() == "openai":
+            self._llm = ChatOpenAI(
+                model=model,
+                temperature=0
+            )
+        else:
+            raise ValueError(f"Provider {provider} not supported. Currently only OpenAI is supported.")
+
     async def generate_message(self, content: str) -> str:
-        # Aquí iría la lógica para generar el mensaje usando un modelo de lenguaje
-        return f"Generated message based on: {content}"
+        return self._llm.invoke(content)
+    
+    def get_llm(self):
+        return self._llm
